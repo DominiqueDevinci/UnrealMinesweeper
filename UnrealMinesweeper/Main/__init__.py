9a6a76d5cbe9f02cbf5b2848d11d76b312278f91
DominiqueDevinci <dominique.blaze@devinci.fr>
2017-12-05 19:39:07 +0100

    Enhanced binomial coeff calculator

--- UnrealMinesweeper/Main/__init__.py
+++ UnrealMinesweeper/Main/__init__.py
@@ -8,21 +8,6 @@ from BoardController import BoardController
 
 import Solver
 
-def choose(n, k):
-    """
-    A fast way to calculate binomial coefficients by Andrew Dalke (contrib).
-    10x faster than scipy.misc.comb
-    """
-    if 0 <= k <= n:
-        ntok = 1
-        ktok = 1
-        for t in xrange(1, min(k, n - k) + 1):
-            ntok *= n
-            ktok *= t
-            n -= 1
-        return ntok // ktok
-    else:
-        return 0
 
 class QPushButtonCustom(QPushButton):
     def __init__(self, id):
